db3b973cc157ea80d95058f09879f2317e50c3d9
maathavan27 <33641294+maathavan27@users.noreply.github.com>
2021-07-24 17:40:40 -0400

    player can be attacked and killed

--- SlasherGame/Assets/MoveEnemy.cs
+++ SlasherGame/Assets/MoveEnemy.cs
@@ -2,21 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveEnemy : MonoBehaviour
-{
+public class MoveEnemy : MonoBehaviour {
     public Transform player;
     private Rigidbody2D rb;
+    private int attackDamage = 20;
+    public float attackRange = 1f;
+    public LayerMask playerLayer;
+    public Transform attackPoint;
+    public float attackRate = 2f;
+    private float nextAttackTime = 0f;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         rb = GetComponent<Rigidbody2D>();
         rb.freezeRotation = true;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         float speed = 0.1f;
         Vector3 playerPos = player.transform.position;
         Vector3 thisPos = transform.position;
@@ -24,23 +27,38 @@ public class MoveEnemy : MonoBehaviour
         float diffX = playerPos[0] - thisPos[0]; //+ = right of enemy
         float diffY = playerPos[1] - thisPos[1]; //+ = top of enemy
         float moveX = 0, moveY = 0;
-        if (diffX > 0)
-        {
+        if (diffX > 0) {
             moveX = 1;
-        }
-        else if (diffX < 0)
-        {
+        } else if (diffX < 0) {
             moveX = -1;
         }
-        if (diffY > 0)
-        {
+        if (diffY > 0) {
             moveY = 1;
-        }
-        else if (diffY < 0)
-        {
+        } else if (diffY < 0) {
             moveY = -1;
         }
         Vector3 moveDir = new Vector3(moveX, moveY).normalized;
         rb.MovePosition(transform.position + moveDir * speed);
+
+        if (Time.time > nextAttackTime) {
+            attack();
+            nextAttackTime = Time.time + 1f / attackRate;
+        }
     }
+
+    void attack() {
+        //play attack animation
+        Collider2D[] hitPlayers = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, playerLayer);
+        if (hitPlayers.Length > 0) {
+            Debug.Log("you got hit");
+            hitPlayers[0].GetComponent<PlayerHealth>().TakeDamage(attackDamage);
+
+        }
+    }
+
+    /*private void OnDrawGizmosSelected() {
+        if (attackPoint != null) {
+            Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        }
+    }*/
 }
---
+++ SlasherGame/Assets/PlayerHealth.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage) {
+        currentHealth -= damage;
+        //play take damage animation
+
+        if (currentHealth <= 0) {
+            //play death animation
+            Debug.Log("you are dead");
+
+            //go to game over screen
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            GetComponent<BoxCollider2D>().enabled = false;
+            GetComponent<MovePlayer>().enabled = false;
+        }
+    }
+
+}
